a8b1801e021f7c0290853dab8ba4cc258a06c215
mcook42 <speedymaco@gmail.com>
2016-08-19 12:30:04 -0600

    Revert "Added functionality to Py to R converter"
    This reverts commit cf041acb2d066b6d32bf2c2291d599bc3b5b96ae.

--- py2r-syntax-converter.py
+++ py2r-syntax-converter.py
@@ -13,72 +13,28 @@ What it does convert:
     -':' to '{'
     -'not' to '!'
     -if statments
-    -rename variables in R style ('_' to '.')
     -add closing brackets on a newline
-    -integrate.quad to integrate
 
 What it doesn't do:
     -Python specific functions to R specific functions
     -Add closing brackets with perfect indentation
-
-# Update 8/12/16
--Added conversion from python single integration
--Fixed if statement bug
--Added variable renaming
 
 TODO: Closing brackets indentation issue
 """
 
 from os import path
 from shutil import copyfile
-import re
 
 # Define changes to make
-simple_change = {"**": "^",
-                 " = ": " <- ",
-                 ":\n": "{\n",
-                 "not": "!"}
+simple_change = {"**" : "^",
+                 " = " : " <- ",
+                 ":\n" : "{\n",
+                 "not" : "!"}
 
 complex_change = {"def " : "",
                   "+=" : '+',
                   "-=" : '-',
-                  "*=" : '*',
-                  "quad(" : "integrate("}
-
-# Define dictionary for variable name changes
-newvars = {}
-
-# Define regular expressions for variable length changes
-# 'integrate(integrand, lower = , upper = )'
-
-#re1 = '(integrate)'	      # integrate (keyword)
-#re2 = '(\\()'	            # open paren
-#re3 = '((?:[a-z][a-z]+))'	 # Word 2
-#re4 = '(,)'	                  # comma
-#re5 = '.*?'	                  # Non-greedy match on filler
-#re6 = '(lower)'	            # lower (keyword)
-#re8 = '(=)'	                  # =
-#re12 = '(upper)'	            # upper (keyword)
-#re16 = '(\\))'	            # )
-#
-#rgx_integrate = re.compile(
-#                            re1 +
-#                            re2 +
-#                            re3 +
-#                            re4 +
-#                            re5 +
-#                            re6 +
-#                            re5 +
-#                            re8 +
-#                            re5 +
-#                            re4 +
-#                            re5 +
-#                            re12 +
-#                            re5 +
-#                            re8 +
-#                            re5 +
-#                            re16,
-#                            re.IGNORECASE | re.DOTALL)
+                  "*=" : '*'}
 
 # Create flag stacks
 flags = {}
@@ -103,7 +59,7 @@ def createRfile():
         filename = input("Please copy and paste the path to your file here: ")
 
     # Test for valid file path
-    if path.exists(filename):
+    if path.exists(filename) :
         #  Strip the directory from the filename
         new_name = path.basename(filename)
         # Replace python extention with R extension
@@ -151,38 +107,15 @@ def complexchange(line):
     for key in complex_change:
         if key in line:
             if key == 'def ' and line.lstrip().startswith(key):
-                # remove 'def' keyword
+                #remove 'def' keyword
                 change = complex_change[key]
                 line_r = ignoreStrReplace(line, key, change)
-                # split function definition at '('
+                #split function definition at '('
                 lsplit = line_r.split('(', maxsplit=1)
                 fname = lsplit[0]
                 params = '(' + lsplit[1]
                 # create R style function def "fname <- function(params)"
                 line = fname + ' <- function' + params
-            elif key == 'quad(' :
-                # Replace keyword
-                change = complex_change[key]
-                line = line.replace(key, change)
-                # Split function call at commas
-                lsplit = line.split(',', maxsplit=2)
-                left = lsplit[0]
-                lower = ' lower = ' + lsplit[1]
-                upper = ' upper = ' + lsplit[2]
-                if 'args' in line:
-                    asplit = line.split('=', maxsplit=1)
-                    args = asplit[1].replace(')\n', '')
-                    left = left + args
-                    upper = upper.replace('args=', '')
-                    #If space inbetween args and =
-                    upper = upper.replace('args =', '')
-                    upper = upper.replace(args, '')
-                    #Remove comma and extra space
-                    upper = upper.replace(')', '')
-                    upper = upper.rstrip()
-                    upper = upper.rstrip(',')
-                    upper = upper + ')\n'
-                line = left + ',' + lower + ',' + upper
             else:
                 line = opfunc(line, key)
     return line
@@ -199,7 +132,6 @@ def chglinecontent(line):
             line = ignoreStrReplace(line, key, simple_change[key])
     line = complexchange(line)
     line = statement(line)
-    line = renameVars(line)
     return line
 
 
@@ -214,7 +146,7 @@ def opfunc(line, op):
     """
     Replaces python operations ('*'=) in line(self) with R style operations.
     """
-    # Check if the operation is contained in a string, don't modify if true.
+    #Check if the operation is contained in a string, don't modify if true.
     check_str_s, check_str_d = stringify(op)
     if not check_str_d in line and not check_str_s in line:
         # Get R style operation
@@ -230,7 +162,6 @@ def opfunc(line, op):
         line = ls + rs
 
     return line
-
 
 def stringify(sub):
     """Returns python string versions ('' and "") of original substring"""
@@ -261,17 +192,24 @@ def standind(line, cur_ind):
     return indentation(line), line
 
 
-# TESTED-WORKS
+#TESTED-WORKS
 def statement(line):
     """Converts if statements"""
-    if 'elif ' in line:
-        line = line.replace('elif', 'else if')
-    if "if " in line:
+    if "if " in line and not 'elif' in line:
         lsplit = line.split('if ', maxsplit=1)
         ls = lsplit[0] + 'if '
         rs = lsplit[1]
         # Replace the ':' at the end of the statement
-        rs = lsplit[1].replace(':', '{')
+        rs = lsplit[1].replace(':','{')
+        rs = '(' + rs
+        rs = rs.replace('{', '){')
+        line = ls + rs
+    if 'elif ' in line:
+        lsplit = line.split('if ', maxsplit=1)
+        ls = lsplit[0] + 'else if '
+        rs = lsplit[1]
+        # Replace the ':' at the end of the statement
+        rs = lsplit[1].replace(':','{')
         rs = '(' + rs
         rs = rs.replace('{', '){')
         line = ls + rs
@@ -281,18 +219,18 @@ def statement(line):
 def ignoreStrReplace(line, cur, rep):
     """Wrapper for str.replace to ignore strings"""
     if '"' in line:
-        # Split string at quotation marks
+        #Split string at quotation marks
         lsplit = line.split('"')
-        # Replace items contained within even partitions
+        #Replace items contained within even partitions
         lsplit[::2] = [spl.replace(cur, rep) for spl in lsplit[::2]]
-        # Rejoin the partitions
+        #Rejoin the partitions
         line = '"'.join(lsplit)
     elif "'" in line:
-        # Split string at quotation marks
+        #Split string at quotation marks
         lsplit = line.split("'")
-        # Replace items contained within even partitions
+        #Replace items contained within even partitions
         lsplit[::2] = [spl.replace(cur, rep) for spl in lsplit[::2]]
-        # Rejoin the partitions
+        #Rejoin the partitions
         line = '"'.join(lsplit)
     else:
         line = line.replace(cur, rep)
@@ -308,17 +246,17 @@ def closeBrackets(file):
         sameBlock = True
         # Ignore lines with only whitespace
         if not line.isspace():
-            # Look for opening brackets if closing brackets remain
+            #Look for opening brackets if closing brackets remain
             if '{\n' in line and flags['bracket']:
                 # Store current index for later
                 i_temp = i
                 # Get starting indentation
                 start_indent = indentation(line)
 
-                while i + 1 < len(file) and sameBlock:
-                    # Get next line, and strip trailing whitespace
-                    nextline = file[i + 1].rstrip()
-                    # Get its indentation
+                while i+1 < len(file) and sameBlock:
+                    #Get next line, and strip trailing whitespace
+                    nextline = file[i+1].rstrip()
+                    #Get its indentation
                     next_ind = indentation(nextline)
                     # Check for decreased indentation and comma continuation
                     if start_indent >= next_ind and not line.endswith(','):
@@ -335,32 +273,6 @@ def closeBrackets(file):
     return file
 
 
-def renameVars(line):
-    if '<-' in line:
-        lsplit = line.split('<-', maxsplit=1)
-        var = lsplit[0]
-        # Check if unpacking into variables
-        if ',' not in var:
-            newvar = var.replace('_', '.')
-            # Strip whitespace
-            newvar = newvar.strip()
-            var = var.strip()
-            newvars.update({var : newvar})
-        else:
-            varnames = line.split(',')
-            for var in varnames:
-                newvar = var.replace('_', '.')
-                # Strip whitespace
-                newvar = newvar.strip()
-                var = var.strip()
-                newvars.update({var : newvar})
-    if newvars:
-        for key in newvars:
-            line = line.replace(key, newvars[key])
-    
-    return line
-
-
 def main():
     pyfile, rfile = createRfile()
 
@@ -373,17 +285,17 @@ def main():
         for line in lines:
             # Get indentation current before adjustment
             indents['cur'] = indentation(line)
-            # Adjust to standard indent
+            #Adjust to standard indent
             indents['cur'], line = standind(line, indents['cur'])
 
-            # Strip whitespace and check for python comment or import
-            if not line.lstrip().startswith(("#", "import", "from")):
+            #Strip whitespace and check for python comment or import
+            if not line.lstrip().startswith(("#","import","from")):
                 # Set the flags for further changes
                 setflags(line, indents)
                 if not flags['comment']:
                     # Perform line changes
                     line = chglinecontent(line)
-            # statement() may need two passes to correctly modify the line
+            #statement() may need two passes to correctly modify the line
             line = statement(line)
             # If the line isn't whitespace, write it to the file
             if not line.isspace():
@@ -407,11 +319,6 @@ def main():
             with open(rfile, "w") as file:
                 # Overwrite old files
                 for line in lines:
-                    if line.lstrip().startswith('#'):
-                        # Fix issue with readability (add newlines)
-                        line = '\n' + line
-                    if '}' in line:
-                        line = line + '\n'
                     file.write(line)
 
 
